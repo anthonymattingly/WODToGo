4077db7a1894761798553e72af1e41c4f7209d71
anthonymattingly <amattingly100@gmail.com>
2017-06-12 19:25:47 -0400

    Added new jquery files and css styling to columns

---
+++ app.js
@@ -0,0 +1,25 @@
+/*start of wod to go JS*/
+
+
+/*
+var movement1 = movement2 = movement3 = [
+    "25 Air Squats",
+    "15 Burpees",
+    "30 Push Ups",
+    "50 Sit Ups",
+    "25 Lunges",
+    "1 minute Forearm Plank",
+    "30 Second Max Effort Tuck Jump"
+];
+
+var rand1 = Math.floor(Math.random() * movement1.length);
+var rand2 = Math.floor(Math.random() * movement2.length);
+var rand3 = Math.floor(Math.random() * movement3.length);
+
+var workoutPrintout = movement1[rand1] + "////" + movement2[rand2] + "////" + movement3[rand3];
+
+alert(workoutPrintout);
+*/
+
+
+
---
+++ externalJS.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".leftButton").click(function(){
+        $(".armLeft").slideUp("fast");
+    });
+});
+
+$(document).ready(function(){
+    $(".middleButton").click(function(){
+        $(".armMiddle").slideUp("fast");
+    });
+});
+
+$(document).ready(function(){
+    $(".rightButton").click(function(){
+        $(".armRight").slideUp("fast");
+    });
+});
